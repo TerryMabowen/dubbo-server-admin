68882c912dcdf238fc81ef6dec02ecc250eb9949
mabowen <mabw@bell.ai>
2020-07-01 10:26:28 +0800

    fixed

---
+++ src/main/java/ai/bell/dubbo/server/admin/DubboAdminApplication.java
@@ -0,0 +1,13 @@
+package ai.bell.dubbo.server.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ai/bell/dubbo/server/admin/DubboAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.bell.dubbo.server.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
